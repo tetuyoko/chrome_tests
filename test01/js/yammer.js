d251acf28bde07c7bc803a4dbe6f27786197d030
Akira Osada <osd.akira@gmail.com>
2014-07-19 19:21:38 +0900

    get image

--- test01/js/yammer.js
+++ test01/js/yammer.js
@@ -1,9 +1,47 @@
+function createImage(attachment){
+    return {
+        thumbnail_url: attachment.thumbnail_url,
+        download_url: attachment.download_url
+    }
+    // download_url;
+    // thumbnail_url
+    // large_preview_url
+}
+
+function toHTML(image){
+    return "<a href='" + image.download_url +  "'>" +
+        "<img src='" + image.thumbnail_url + "'>" +
+        "</a>" +
+        "<br>";
+}
+
 $(function(){
-    // var url = "https://www.yammer.com/mugenup.com/#/threads/inGroup?type=in_group&feedId=4450127";
-    var url = "https://www.yammer.com/api/v1/messages.json";
+    var groupID = "4450127";
+    var url = "https://www.yammer.com/api/v1/messages/in_group/"+ groupID + ".json";
     $.get(url, function(data, status, xhr){
-        $(document.body).replaceWith(data);
-        console.log(data);
-        window._m_data = data;
+        // console.log(data);
+        // window._data = data;
+        var images = [];
+        data.messages.forEach(function(message){
+            if(message.attachments){
+                message.attachments.forEach(function(attachment){
+                    // console.log(attachment);
+                    images.push(createImage(attachment));
+                });
+            }
+        });
+        var image_tags = [];
+        images.forEach(function(image){
+            image_tags.push(toHTML(image));
+        })
+        $(document.body).append(image_tags);
+    });
+
+    $(document).on("click", "a", function(e){
+        $this = $(this);
+        $.get($this.attr("href"), function(data, status, xhr){
+            // console.log(status);
+            var data = data;
+        });
     });
 });
