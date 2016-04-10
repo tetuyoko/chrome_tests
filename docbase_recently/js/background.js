6d7c8278bfcf4e8b9ab369278d78b32ed4adffd1
Akira Osada <osd.akira@gmail.com>
2016-04-10 14:15:10 +0900

    GC

--- docbase_recently/js/background.js
+++ docbase_recently/js/background.js
@@ -1,41 +1,4 @@
 /****
-* アイコンのクリックで、モードをオン・オフする
-* content_script の中からは、chrome.storage.local で状態を取得する
-****/
-var clicks = 1;
-var toggleEnabled = function(){
-  clicks += 1;
-  return isEnabled();
-}
-var isEnabled = function(){
-  return !!(clicks % 2);
-}
-var getIconPath = function(enabled){
-  var iconName = enabled ? "icon-hidden-96.png" : "icon-96.png";
-  return "./images/" + iconName;
-}
-var setIcon = function(enabled, tab){
-  var iconPath = getIconPath(enabled);
-  chrome.pageAction.setIcon({ path: iconPath, tabId: tab.id });
-}
-
-// アイコンのクリックでモードを切り替える
-chrome.pageAction.onClicked.addListener(function(tab) {
-  var enabled = toggleEnabled();
-  setIcon(enabled, tab);
-  chrome.storage.local.set({ 'enabled': enabled })
-});
-
-// 該当のURLを開いたら、アイコンを有効化する
-chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-  if (tab.url.indexOf("https://mugenup.docbase.io") > -1) {
-    chrome.pageAction.show(tabId);
-    var enabled = isEnabled();
-    setIcon(enabled, tab);
-  }
-});
-
-/****
 * ダッシュボードの件数を20件にするハック
 ****/
 // http://mzsm.me/2012/02/10/chrome17-webrequest-api/
@@ -61,13 +24,13 @@ chrome.webRequest.onBeforeRequest.addListener(
 ****/
 chrome.webRequest.onCompleted.addListener(
   function(details) {
+    // backgroud から content へは、sendMessage は使えない。sendMessage を使う
+    // http://stackoverflow.com/questions/14245334/chrome-extension-sendmessage-from-background-to-content-script-doesnt-work
+    //chrome.runtime.sendMessage({ type: "isGroupsPage" }, function(response) {});
     var url = details.url;
     chrome.tabs.getSelected(null, function(tab) {
       chrome.tabs.sendMessage(tab.id, {type: "isGroupsPage"}, function(response) {});
     });
-    // backgroud から content へは、sendMessage は使えない
-    // http://stackoverflow.com/questions/14245334/chrome-extension-sendmessage-from-background-to-content-script-doesnt-work
-    //chrome.runtime.sendMessage({ type: "isGroupsPage" }, function(response) {});
   },
   {
     urls: ["*://mugenup.docbase.io/groups/*"],
--- docbase_recently/js/content_script.js
+++ docbase_recently/js/content_script.js
@@ -1,5 +1,9 @@
+/****
+* グループページに開閉ボタンを付ける。
+****/
+
 var subMenuHider = function(){
-  var $toggler = $('<div style="position:absolute;top:0;background:yellow;z-index:1;width:10px;height:20px;"></div>');
+  var $toggler = $('<div style="position:absolute;top:0;background:yellow;z-index:1;width:10px;height:20px;cursor:pointer;"></div>');
   var $closer = $toggler.clone().css({"right": 0}).text("<");
   var $opener = $toggler.clone().css({"left": 0, "display": "none"}).text(">");
   var $subMenu = $(".js-main-sub-menu");
@@ -20,7 +24,6 @@ var subMenuHider = function(){
 }
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-  // console.log("receive message");
   switch (request.type) {
     case "isGroupsPage":
       subMenuHider();
@@ -28,34 +31,3 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
       break;
   }
 });
-
-// var actionIfEnabled = function(action) {
-//   return function() {
-//     chrome.storage.local.get('enabled', function (value) {
-//       var enabled = value.enabled;
-//       if (enabled === void 0) {
-//         enabled = true;
-//       }
-//       if(enabled){
-//         action()
-//       }
-//     })
-//   }
-// }
-// var jumpToRecent = function() {
-//   if(location.pathname == "/"){
-//    var timer = setInterval(function(){
-//      $link = $('a[href="/recent"]');
-//      if(!!$link[0]){
-//        clearInterval(timer);
-//        $link.click();
-//      }
-//    }, 100)
-//   }
-// }
-// read では初回しか反応しないのでダメ
-// ajaxStop も ajaxComplete も、キャッチできなかった
-//$(document).on("ready", actionIfEnabled(jumpToRecent));
-// $(document).on("ready", function(e){
-//   console.log("aaaaaaa");
-// });
