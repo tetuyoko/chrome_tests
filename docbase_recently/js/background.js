57ceb405e2e4d93cb4629f1841dbbfe3234f54a5
Akira Osada <osd.akira@gmail.com>
2016-04-10 11:34:36 +0900

    ダッシュボードの件数を20件にする

--- docbase_recently/js/background.js
+++ docbase_recently/js/background.js
@@ -1,3 +1,7 @@
+/****
+* アイコンのクリックで、モードをオン・オフする
+* content_script の中からは、chrome.storage.local で状態を取得する
+****/
 var clicks = 1;
 var toggleEnabled = function(){
   clicks += 1;
@@ -7,30 +11,49 @@ var isEnabled = function(){
   return !!(clicks % 2);
 }
 var getIconPath = function(enabled){
-  if (enabled) {
-    return "./images/icon-hidden-96.png";
-  }
-  else {
-    return "./images/icon-96.png";
-  }
+  var iconName = enabled ? "icon-hidden-96.png" : "icon-96.png";
+  return "./images" + iconName;
 }
 var setIcon = function(enabled, tab){
   var iconPath = getIconPath(enabled);
   chrome.pageAction.setIcon({ path: iconPath, tabId: tab.id });
 }
 
+// アイコンのクリックでモードを切り替える
 chrome.pageAction.onClicked.addListener(function(tab) {
   var enabled = toggleEnabled();
   setIcon(enabled, tab);
   chrome.storage.local.set({ 'enabled': enabled })
 });
 
-function checkForValidUrl(tabId, changeInfo, tab) {
+// 該当のURLを開いたら、アイコンを有効化する
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
   if (tab.url.indexOf("https://mugenup.docbase.io") > -1) {
     chrome.pageAction.show(tabId);
     var enabled = isEnabled();
     setIcon(enabled, tab);
-    console.log(enabled);
   }
-};
-chrome.tabs.onUpdated.addListener(checkForValidUrl);
+});
+
+/****
+* ダッシュボードの件数を20件にするハック
+****/
+
+// http://mzsm.me/2012/02/10/chrome17-webrequest-api/
+var availableURLs = ["*://mugenup.docbase.io/*"];
+chrome.webRequest.onBeforeRequest.addListener(
+  function(details) {
+    var url = details.url;
+    if (url.indexOf("/api/dashboard_objects?per_page=5&page=1") > -1) {
+      var new_url = url.replace("per_page=5", "per_page=20");
+      return { redirectUrl: new_url }
+    }
+  },
+  {
+    urls: availableURLs,
+    types: ["xmlhttprequest"]
+  },
+  [
+    "blocking"
+  ]
+);
--- docbase_recently/js/content_script.js
+++ docbase_recently/js/content_script.js
@@ -1,25 +1,30 @@
-var actionIfEnabled = function(action) {
-  return function() {
-    chrome.storage.local.get('enabled', function (value) {
-      var enabled = value.enabled;
-      if (enabled === void 0) {
-        enabled = true;
-      }
-      if(enabled){
-        action()
-      }
-    })
-  }
-}
-var jumpToRecent = function() {
-  if(location.pathname == "/"){
-   var timer = setInterval(function(){
-     $link = $('a[href="/recent"]');
-     if(!!$link[0]){
-       clearInterval(timer);
-       $link.click();
-     }
-   }, 100)
-  }
-}
-$(document).on("ready", actionIfEnabled(jumpToRecent));
+// var actionIfEnabled = function(action) {
+//   return function() {
+//     chrome.storage.local.get('enabled', function (value) {
+//       var enabled = value.enabled;
+//       if (enabled === void 0) {
+//         enabled = true;
+//       }
+//       if(enabled){
+//         action()
+//       }
+//     })
+//   }
+// }
+// var jumpToRecent = function() {
+//   if(location.pathname == "/"){
+//    var timer = setInterval(function(){
+//      $link = $('a[href="/recent"]');
+//      if(!!$link[0]){
+//        clearInterval(timer);
+//        $link.click();
+//      }
+//    }, 100)
+//   }
+// }
+// read では初回しか反応しないのでダメ
+// ajaxStop も ajaxComplete も、キャッチできなかった
+//$(document).on("ready", actionIfEnabled(jumpToRecent));
+// $(document).on("ready", function(e){
+//   console.log("aaaaaaa");
+// });
