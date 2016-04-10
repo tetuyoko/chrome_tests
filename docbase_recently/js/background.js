b3474dbab6aac684cc7938d0cf4b53e22b0ce974
Akira Osada <osd.akira@gmail.com>
2016-04-10 12:21:18 +0900

    サブメニューの開く、閉じるボタンを付ける

--- docbase_recently/js/background.js
+++ docbase_recently/js/background.js
@@ -12,7 +12,7 @@ var isEnabled = function(){
 }
 var getIconPath = function(enabled){
   var iconName = enabled ? "icon-hidden-96.png" : "icon-96.png";
-  return "./images" + iconName;
+  return "./images/" + iconName;
 }
 var setIcon = function(enabled, tab){
   var iconPath = getIconPath(enabled);
@@ -38,22 +38,39 @@ chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
 /****
 * ダッシュボードの件数を20件にするハック
 ****/
-
 // http://mzsm.me/2012/02/10/chrome17-webrequest-api/
-var availableURLs = ["*://mugenup.docbase.io/*"];
 chrome.webRequest.onBeforeRequest.addListener(
   function(details) {
     var url = details.url;
-    if (url.indexOf("/api/dashboard_objects?per_page=5&page=1") > -1) {
+    if (url.indexOf("per_page=5&page=1") > -1) {
       var new_url = url.replace("per_page=5", "per_page=20");
       return { redirectUrl: new_url }
     }
   },
   {
-    urls: availableURLs,
+    urls: ["*://mugenup.docbase.io/api/dashboard_objects"],
     types: ["xmlhttprequest"]
   },
   [
     "blocking"
   ]
+);
+
+/****
+* グループの表示の時に、拡張を割りこませる
+****/
+chrome.webRequest.onCompleted.addListener(
+  function(details) {
+    var url = details.url;
+    chrome.tabs.getSelected(null, function(tab) {
+      chrome.tabs.sendMessage(tab.id, {type: "isGroupsPage"}, function(response) {});
+    });
+    // backgroud から content へは、sendMessage は使えない
+    // http://stackoverflow.com/questions/14245334/chrome-extension-sendmessage-from-background-to-content-script-doesnt-work
+    //chrome.runtime.sendMessage({ type: "isGroupsPage" }, function(response) {});
+  },
+  {
+    urls: ["*://mugenup.docbase.io/groups"],
+    types: ["xmlhttprequest"]
+  }
 );
--- docbase_recently/js/content_script.js
+++ docbase_recently/js/content_script.js
@@ -1,3 +1,34 @@
+var subMenuHider = function(){
+  var $toggler = $('<div style="position:absolute;top:0;background:yellow;z-index:1;width:10px;height:20px;"></div>');
+  var $closer = $toggler.clone().css({"right": 0}).text("<");
+  var $opener = $toggler.clone().css({"left": 0, "display": "none"}).text(">");
+  var $subMenu = $(".js-main-sub-menu");
+
+  $(".sub-menu").prepend($closer);
+  $(".js-main-content").prepend($opener);
+
+  $closer.on("click", function(){
+    $subMenu.hide();
+    $opener.show();
+    $(window).trigger("resize");
+  })
+  $opener.on("click", function(){
+    $subMenu.show();
+    $opener.hide();
+    $(window).trigger("resize");
+  })
+}
+
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+  // console.log("receive message");
+  switch (request.type) {
+    case "isGroupsPage":
+      subMenuHider();
+      sendResponse({});
+      break;
+  }
+});
+
 // var actionIfEnabled = function(action) {
 //   return function() {
 //     chrome.storage.local.get('enabled', function (value) {
